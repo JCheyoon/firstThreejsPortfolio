c3c5865cdea29daa1ed0b0361f5dcc12d455daba
Cheyoon Jung <jessietwo94@gmail.com>
2023-08-21 19:51:01 +0200

    change board

--- src/models/Board.jsx
+++ src/models/Board.jsx
@@ -10,21 +10,11 @@ const Board = () => {
       <RigidBody
         colliders="trimesh"
         type="fixed"
-        position={[0, 0, -9]}
+        position={[0, 1, -9]}
         rotation={[0, -Math.PI / 2, 0]}
         scale={[0.8, 0.8, 0.8]}
       >
         <BoardModel />
-        <Html
-          transform
-          wrapperClass="htmlScreen"
-          distanceFactor={1.4}
-          scale={[1.29, 1.06, 1.3]}
-          position={[0, 2.0, -0.09]}
-          rotation-y={Math.PI / 2}
-        >
-          <iframe src="https://jcheyoon.com/" />
-        </Html>
       </RigidBody>
     </group>
   );
